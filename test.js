ed5a244096f914bdcf25d5bb7ef1e6903aab0677
shabnambegum123 <shabnambegum227@gmail.com>
2024-07-01 20:37:06 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,98 +1,32 @@
-// let prompt = require('prompt-sync')()
+const express = require('express')
+let app = express()
+const bodyparser = require('body-parser')
+app.use(bodyparser.json())
+const router = express.Router()
+app.use(router)
 
-// function data(num1, num2, num3) {
+ const fs = require('fs')
+ const path = require('path')
 
-//    console.log("num1", typeof num2)
+router.post('/test/user', (req, res) => {
+    let input = req.body
+    console.log(input)
 
-//     if (typeof num1 === 'string' && typeof num2 === 'string' && typeof num3 === 'string') {
+    
+let filePath = path.join(__dirname + "/shabnam.js")
 
-//         let a = Number(num1);
-//         let  b = Number(num2);
-//          let c = Number(num3);
-//         let multiply = a * b * c
+console.log(filePath)
+let result = JSON.stringify(input)
+    fs.writeFile(filePath,result, 'Utf-8', (err) => {
+        if (err) console.log(err)
+        else console.log("sucess")
+    })
+    res.send('sended')
+    
+})
+let PORT = 3456;
 
-//         console.log(multiply);
-//     }
-//     else {
-//         console.log("String only allowed!....")
-//     }
-// }
-
-// let value = prompt("Enter the value 1 ")
-
-// let value2 = prompt("Enter the value 2 ")
-
-// let value3 = prompt("Enter the value 3 ")
-
-
-
-// data(value, value2, value3);
-
-
-
-// let prompt = require('prompt-sync')()
-// function data (num1,num2){
-//     console.log(num1+num2);
-//     if(data){
-//         let a = num1
-//         let b = num2
-//         let add = a+b;
-//         console.log(add);
-//     }
-
-
-// }
-
-
-// let value = prompt("Enter the value 1")
-// let value2 = prompt("Enter the value 2")
-
-// data (value,value2)
-
-
-
-// let data =async () => {
-// console.log("test")
-
-//  setTimeout (() => {
-//     console.log("hii")
-// }, 1000)
-
-
-
-// }
-
-// data()
-
-let data = async () => {
-    console.log("test");
-
-    await new Promise(resolve => {
-        setTimeout(() => {
-            console.log("hiiiii");
-            resolve();
-        }, 0);
-        setImmediate(() => {
-            console.log("hii");
-            resolve();
-        });
-    });
-
-    console.log("qwegjqrhq");
-};
-
-data();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`server is looking for ${PORT}`)
+}
+)
